713b7caeccdea1f23a8e97e1176064eac95d1b1a
katherine <katherinejc103@gmail.com>
2022-09-25 21:12:10 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,20 @@
+class Book 
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+
+    def initialize(title)
+        @title = title
+        #@author = author
+        #@pageCount = pageCount
+       # @genre = genre 
+
+    end
+        def turn_page
+            puts "Flipping the page...wow, you read fast!"
+        end
+end 
+
 
 
